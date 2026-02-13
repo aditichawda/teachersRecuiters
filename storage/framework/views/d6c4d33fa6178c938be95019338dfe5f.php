cfa348dc55e8373cc6bee7f7ceae2f08f4764444
Himanshi-Vyas <himanshivyas2005@gmail.com>
2026-02-13 11:10:21 +0530

    13 feb comment

---
+++ storage/framework/views/d6c4d33fa6178c938be95019338dfe5f.php
@@ -0,0 +1,87 @@
+<?php $__env->startPush('footer'); ?>
+    <?php if (isset($component)) { $__componentOriginaldc8ac54b6bf7eb0d0560fdd5aa630687 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginaldc8ac54b6bf7eb0d0560fdd5aa630687 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => '8def1252668913628243c4d363bee1ef::modal','data' => ['id' => 'add-company-modal','title' => trans('plugins/job-board::messages.create_a_company'),'hasForm' => true]] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('core::modal'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['id' => 'add-company-modal','title' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute(trans('plugins/job-board::messages.create_a_company')),'has-form' => true]); ?>
+        <?php if (isset($component)) { $__componentOriginal50e5e771b30c35423d2b4f118feb7c0c = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal50e5e771b30c35423d2b4f118feb7c0c = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => '8def1252668913628243c4d363bee1ef::form.label','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('core::form.label'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?><?php echo e(trans('core/base::forms.name')); ?> <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal50e5e771b30c35423d2b4f118feb7c0c)): ?>
+<?php $attributes = $__attributesOriginal50e5e771b30c35423d2b4f118feb7c0c; ?>
+<?php unset($__attributesOriginal50e5e771b30c35423d2b4f118feb7c0c); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal50e5e771b30c35423d2b4f118feb7c0c)): ?>
+<?php $component = $__componentOriginal50e5e771b30c35423d2b4f118feb7c0c; ?>
+<?php unset($__componentOriginal50e5e771b30c35423d2b4f118feb7c0c); ?>
+<?php endif; ?>
+        <?php if (isset($component)) { $__componentOriginala5b2ce8ea835a1a6ed10854da20fa051 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginala5b2ce8ea835a1a6ed10854da20fa051 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => '8def1252668913628243c4d363bee1ef::form.text-input','data' => ['name' => 'name','type' => 'text','id' => 'company_name','placeholder' => trans('core/base::forms.name')]] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('core::form.text-input'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['name' => 'name','type' => 'text','id' => 'company_name','placeholder' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute(trans('core/base::forms.name'))]); ?>
+             <?php $__env->slot('append', null, []); ?> 
+                <?php if (isset($component)) { $__componentOriginal922f7d3260a518f4cf606eecf9669dcb = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal922f7d3260a518f4cf606eecf9669dcb = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => '8def1252668913628243c4d363bee1ef::button','data' => ['type' => 'submit','color' => 'primary','id' => 'btn-add-company']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('core::button'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['type' => 'submit','color' => 'primary','id' => 'btn-add-company']); ?>
+                    <?php echo e(trans('plugins/job-board::general.save')); ?>
+
+                 <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal922f7d3260a518f4cf606eecf9669dcb)): ?>
+<?php $attributes = $__attributesOriginal922f7d3260a518f4cf606eecf9669dcb; ?>
+<?php unset($__attributesOriginal922f7d3260a518f4cf606eecf9669dcb); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal922f7d3260a518f4cf606eecf9669dcb)): ?>
+<?php $component = $__componentOriginal922f7d3260a518f4cf606eecf9669dcb; ?>
+<?php unset($__componentOriginal922f7d3260a518f4cf606eecf9669dcb); ?>
+<?php endif; ?>
+             <?php $__env->endSlot(); ?>
+         <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginala5b2ce8ea835a1a6ed10854da20fa051)): ?>
+<?php $attributes = $__attributesOriginala5b2ce8ea835a1a6ed10854da20fa051; ?>
+<?php unset($__attributesOriginala5b2ce8ea835a1a6ed10854da20fa051); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginala5b2ce8ea835a1a6ed10854da20fa051)): ?>
+<?php $component = $__componentOriginala5b2ce8ea835a1a6ed10854da20fa051; ?>
+<?php unset($__componentOriginala5b2ce8ea835a1a6ed10854da20fa051); ?>
+<?php endif; ?>
+        <div class="modal-notice"></div>
+     <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginaldc8ac54b6bf7eb0d0560fdd5aa630687)): ?>
+<?php $attributes = $__attributesOriginaldc8ac54b6bf7eb0d0560fdd5aa630687; ?>
+<?php unset($__attributesOriginaldc8ac54b6bf7eb0d0560fdd5aa630687); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginaldc8ac54b6bf7eb0d0560fdd5aa630687)): ?>
+<?php $component = $__componentOriginaldc8ac54b6bf7eb0d0560fdd5aa630687; ?>
+<?php unset($__componentOriginaldc8ac54b6bf7eb0d0560fdd5aa630687); ?>
+<?php endif; ?>
+<?php $__env->stopPush(); ?>
+<?php /**PATH C:\xampp\htdocs\Aditi\platform\plugins\job-board\/resources/views/partials/add-company.blade.php ENDPATH**/ ?>
